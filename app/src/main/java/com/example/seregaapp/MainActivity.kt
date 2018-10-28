871c0ffa098ceab6716a064f25f40a990ca2ec4f
Zgorbell <zgorbell@tut.by>
2018-10-28 23:43:32 +0300

    initial commit

---
+++ app/src/main/java/com/example/seregaapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.seregaapp
+
+import android.annotation.SuppressLint
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import com.example.seregaapp.ui.MainWebViewClient
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webViewMain: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initWebView()
+        loadUrlWebView(getString(R.string.main_url))
+    }
+
+    override fun onBackPressed() {
+        if (webViewMain.canGoBack()) webViewMain.goBack()
+        else super.onBackPressed()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView(){
+        webViewMain = findViewById(R.id.webViewMain)
+        webViewMain.webViewClient = MainWebViewClient()
+        webViewMain.settings.javaScriptEnabled = true
+    }
+
+    private fun loadUrlWebView(url: String){
+        webViewMain.loadUrl(url)
+    }
+}
