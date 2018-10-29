a273811c1f4a59e227b438b99a8ffbb1589c20bd
Evgenii Veremko <evgenii.veremko@1000geeks.com>
2018-10-29 15:23:49 +0300

    Change version, change url, set fullscreen

--- app/src/main/java/com/example/seregaapp/MainActivity.kt
+++ app/src/main/java/com/example/seregaapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.seregaapp
 import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.webkit.WebView
 import com.example.seregaapp.ui.MainWebViewClient
 
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setFullScreenMode()
         initWebView()
         loadUrlWebView(getString(R.string.main_url))
     }
@@ -31,5 +33,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadUrlWebView(url: String){
         webViewMain.loadUrl(url)
+    }
+
+    private fun setFullScreenMode(){
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
